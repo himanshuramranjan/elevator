4eec8d4cf7839ded22bd888a7be35272af1b44a6
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-02 10:53:43 +0530

    Elevator System added

---
+++ src/model/ElevatorSystem.java
@@ -0,0 +1,56 @@
+package model;
+
+import strategy.elevatorcontrol.ElevatorControllerStrategy;
+import strategy.elevatorselection.ElevatorSelectionStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElevatorSystem {
+
+    public static volatile ElevatorSystem elevatorSystem;
+    private List<ElevatorController> elevatorControllers;
+    private List<Floor> floors;
+    private ElevatorSelectionStrategy elevatorSelectionStrategy;
+    private ElevatorControllerStrategy elevatorControllerStrategy;
+
+    private ElevatorSystem() {
+        this.elevatorControllers = new ArrayList<>();
+        this.floors = new ArrayList<>();
+    }
+
+    public static ElevatorSystem getInstance() {
+        if(elevatorSystem == null) {
+            synchronized (ElevatorSystem.class) {
+                if(elevatorSystem == null) {
+                    elevatorSystem = new ElevatorSystem();
+                }
+            }
+        }
+        return elevatorSystem;
+    }
+
+    public void addElevators(ElevatorController elevatorController) {
+        this.elevatorControllers.add(elevatorController);
+    }
+
+    public void removeElevators(ElevatorController elevatorController) {
+        this.elevatorControllers.remove(elevatorController);
+    }
+
+    public void addFloor(Floor floor) {
+        this.floors.add(floor);
+    }
+
+    public void removeFloor(Floor floor) {
+        this.floors.remove(floor);
+    }
+
+    public void setElevatorSelectionStrategy(ElevatorSelectionStrategy elevatorSelectionStrategy) {
+        this.elevatorSelectionStrategy = elevatorSelectionStrategy;
+    }
+
+    public void setElevatorControllerStrategy(ElevatorControllerStrategy elevatorControllerStrategy) {
+        this.elevatorControllerStrategy = elevatorControllerStrategy;
+    }
+}
