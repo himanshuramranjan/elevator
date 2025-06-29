442966599ca8fff91f44cba038ee8f578cec90e6
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-06-29 22:42:32 +0530

    added the functionality to move the car

--- src/model/ElevatorCar.java
+++ src/model/ElevatorCar.java
@@ -1,5 +1,10 @@
 package model;
 
+import enums.Direction;
+import model.button.Button;
+import model.button.InternalButton;
+import model.buttondispatcher.InternalButtonDispatcher;
+
 public class ElevatorCar {
     private final int id;
     private int curFloorNum;
@@ -12,22 +17,23 @@ public class ElevatorCar {
         this.id = id;
         this.curFloorNum = 0;
         this.display = new Display();
-        this.internalButton = new InternalButton();
+        this.internalButton = new InternalButton(new InternalButtonDispatcher(), id);
         this.elevatorDoor = new ElevatorDoor();
         this.direction = Direction.NONE;
     }
 
     public void move(int floor, Direction direction) {
-        System.out.println("Elevator " + id + "moving " + direction);
-        System.out.println("Elevator " + id + "stops at floor " + floor);
+        setDisplay(direction);
+        System.out.println("Elevator " + id + " moving " + direction);
+        System.out.println("Elevator " + id + " stops at floor " + floor);
         this.elevatorDoor.open(id);
         this.elevatorDoor.close(id);
-        setDisplay(direction);
+        this.curFloorNum = floor;
     }
 
     public void pressButton(int floor) {
-        if(floor > curFloorNum) internalButton.pressButton(floor, Direction.UP, id);
-        else internalButton.pressButton(floor, Direction.DOWN, id);
+        if(floor > curFloorNum) internalButton.pressButton(floor, Direction.UP);
+        else internalButton.pressButton(floor, Direction.DOWN);
     }
 
     private void setDisplay(Direction direction) {
